4442faa517500b1804f346664fe33faa3bdb26fa
CafePraThi <cafeprathi@gmail.com>
2023-03-17 23:07:11 -0300

    url

--- recipes/models.py
+++ recipes/models.py
@@ -5,6 +5,9 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=65)
 
+    def __str__(self):
+        return self.name
+
 
 # Create your models here.
 class Recipe(models.Model):
@@ -24,3 +27,6 @@ class Recipe(models.Model):
     category = models.ForeignKey(
         Category, on_delete=models.SET_NULL, null=True)
     author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.title
