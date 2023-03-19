41cc37690a009183b820bea887de2158525d0140
CafePraThi <cafeprathi@gmail.com>
2023-03-18 22:25:10 -0300

    filter

--- recipes/views.py
+++ recipes/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 
 from recipes.models import Recipe
 from ultils.recipes.factory import make_recipe
+from django.http import Http404
 
 
 def home(request):
@@ -13,8 +14,11 @@ def home(request):
 
 def category(request, category_id):
     recipes = Recipe.objects.filter(category__id=category_id, is_published=True).order_by('-id')
+    if not recipes:
+        raise Http404('Not Found!')
+
     return render(request, 'recipes/pages/category.html', context={
-        'recipes': recipes,
+        'recipes': recipes, 'title': f'{recipes.first().category.name} -  Category |',
     })
 
 
