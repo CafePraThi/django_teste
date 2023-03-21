cc18b17e8b52002b572683d89cd5dffd6eda6d36
CafePraThi <cafeprathi@gmail.com>
2023-03-20 21:21:27 -0300

    error404

--- recipes/views.py
+++ recipes/views.py
@@ -1,8 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import get_list_or_404, render
 
 from recipes.models import Recipe
 from ultils.recipes.factory import make_recipe
-from django.http import Http404
 
 
 def home(request):
@@ -13,17 +12,22 @@ def home(request):
 
 
 def category(request, category_id):
-    recipes = Recipe.objects.filter(category__id=category_id, is_published=True).order_by('-id')
-    if not recipes:
-        raise Http404('Not Found!')
+    recipes = get_list_or_404(
+        Recipe.objects.filter(category__id=category_id,
+                              is_published=True).order_by('-id'))
 
     return render(request, 'recipes/pages/category.html', context={
-        'recipes': recipes, 'title': f'{recipes.first().category.name} -  Category |',
+        'recipes': recipes,
+        'title': f'{recipes[0].category.name} -  Category | ',
     })
 
 
 def recipe(request, id):
+    recipe = Recipe.objects.filter(
+        pk=id,
+        is_published=True,
+    ).order_by('-id').first()
     return render(request, 'recipes/pages/recipe-view.html', context={
-        'recipe': make_recipe(),
+        'recipe': recipe,
         'is_detail_page': True
     })
